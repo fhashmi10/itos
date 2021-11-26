176f4d97467ce52530605fd1c1768dad91586d4f
@fhashmi10 <farazhashm@gmail.com>
2021-11-26 21:17:09 +0530

    updates

--- prediction/predict_itos.py
+++ prediction/predict_itos.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import gc
 
 from pickle import load
 from datetime import datetime
@@ -12,19 +13,6 @@ class PredictItos:
     def __init__(self, config):
         self.units = Config.from_json(config).model.units
         LOG.info(f'Init: {datetime.now().time()}')
-        self.feature_model =tf.keras.models.load_model(
-            'saved_model/inceptionv3', compile=False)
-        LOG.info(f'inception loaded: {datetime.now().time()}')
-        self.encoder = tf.keras.models.load_model(
-            'saved_model/encoder', compile=False)
-        LOG.info(f'Encoder loaded: {datetime.now().time()}')
-        self.decoder = tf.keras.models.load_model(
-            'saved_model/decoder', compile=False)
-        LOG.info(f'Decoder loaded {datetime.now().time()}')
-        self.tokenizer = None
-        with open("saved_model/tokenizer.pkl", "rb") as f:
-            self.tokenizer = load(f)
-        LOG.info(f'Tokenizer loaded: {datetime.now().time()}')
 
     def preprocess(self, image):
         image = tf.image.resize(image, (299, 299))
@@ -34,7 +22,10 @@ class PredictItos:
 
     def get_image_tensor(self, image):
         input_img = self.preprocess(image)
-        img_tensor_val = self.feature_model(input_img)
+        feature_model = tf.keras.models.load_model('saved_model/inceptionv3', compile=False)
+        LOG.info(f'inception loaded: {datetime.now().time()}')
+        img_tensor_val = feature_model(input_img)
+        del feature_model
         img_tensor_val = tf.reshape(
             img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
         return img_tensor_val
@@ -47,23 +38,40 @@ class PredictItos:
 
     def predict(self, image=None):
         img_tensor_val = self.get_image_tensor(image)
-        features = self.encoder(img_tensor_val)
-        dec_input = tf.expand_dims([self.tokenizer.word_index['<start>']], 0)
+        encoder = tf.keras.models.load_model('saved_model/encoder', compile=False)
+        LOG.info(f'Encoder loaded: {datetime.now().time()}')
+        features = encoder(img_tensor_val)
+        del encoder
+        gc.collect()
+        
+        tokenizer = None
+        with open("saved_model/tokenizer.pkl", "rb") as f:
+            tokenizer = load(f)
+        LOG.info(f'Tokenizer loaded: {datetime.now().time()}')
+
+        dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
         hidden = tf.zeros((1, self.units))
         result = []
 
+        decoder =  tf.keras.models.load_model('saved_model/decoder', compile=False)
+        LOG.info(f'Decoder loaded {datetime.now().time()}')
+
+       
+
         for i in range(39):  # todo:correct max length - add programmaticaly
-            predictions, state, attention_weights = self.decoder(dec_input,
+            predictions, state, attention_weights = decoder(dec_input,
                                                                  features,
                                                                  hidden)
             predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
-            result.append(self.tokenizer.index_word[predicted_id])
+            result.append(tokenizer.index_word[predicted_id])
 
-            if self.tokenizer.index_word[predicted_id] == '<end>':
+            if tokenizer.index_word[predicted_id] == '<end>':
                 return self.filter_text(result) 
 
             dec_input = tf.expand_dims([predicted_id], 0)
 
+        del tokenizer
+        del decoder
         return self.filter_text(result)
     
     def predict_service(self, image=None):
